3deef272243b33109aed7915db947c624d5936de
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-11-27 21:12:51 +0100

    Test Fix NewBills

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -12,6 +12,7 @@ describe("Given I am connected as an employee", () => {
     test("Then ...", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
+      
       //to-do write assertion
     })
   })
