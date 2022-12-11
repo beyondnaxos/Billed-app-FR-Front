48b5b54c9b1367e0710f99fc21f7991018679ed7
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-12-11 12:00:25 +0100

    Update NewBill.js

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,23 +2,33 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+import { screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+// import { localStorageMock } from "../__mocks__/localStorage.js"
+// import Router from "../app/Router.js"
+// import { ROUTES_PATH } from "../constants/routes.js"
 
 
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then icon mail should be highlighted ", () => {
+    test("Then icon mail should be highlighted ", async () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      // const verticalLayout = document.querySelector(".vertical-navbar")
-      // const iconMail = verticalLayout.querySelector("#icon-mail")
-      // to-do write assertion
-      expect(iconMail.classList.contains("active-icon")).toBeTruthy()
-
+      // Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      // window.localStorage.setItem('user', JSON.stringify({
+      //   type: 'Employee'
+      // }))
+      // const root = document.createElement("div")
+      // root.setAttribute("id", "root")
+      // document.body.append(root)
+      // Router()
+      // window.onNavigate(ROUTES_PATH.NewBill)
+      // await waitFor(() => screen.getByTestId('icon-window'))
+      // const windowIcon = screen.getByTestId('icon-window')
+      // expect(windowIcon.classList.contains('active-icon')).toBeTruthy()
      
     })
   })
